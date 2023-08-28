58838a8d3138761e617c88b8123fc3bb9d78c0d4
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-08-28 18:19:14 +0700

    ok2

--- index.js
+++ index.js
@@ -16,121 +16,121 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
-// app.get("/api/v1/books", async (req, res) => {
-//   const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
-//   let page = +req.query?.page;
+app.get("/api/v1/books", async (req, res) => {
+  const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
+  let page = +req.query?.page;
 
-//   if (!page || page <= 0) page = 1;
+  if (!page || page <= 0) page = 1;
 
-//   const skip = (page - 1) * +limit;
+  const skip = (page - 1) * +limit;
 
-//   const query = {};
+  const query = {};
 
-//   if (keyword) query.name = { $regex: keyword, $options: "i" };
+  if (keyword) query.name = { $regex: keyword, $options: "i" };
 
-//   try {
-//     const data = await BookModel.find(query)
-//       .skip(skip)
-//       .limit(limit)
-//       .sort({ [orderBy]: sortBy });
-//     const totalItems = await BookModel.countDocuments(query);
-//     return res.status(200).json({
-//       msg: "Ok",
-//       data,
-//       totalItems,
-//       totalPages: Math.ceil(totalItems / limit),
-//       limit: +limit,
-//       currentPage: page,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
+  try {
+    const data = await BookModel.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort({ [orderBy]: sortBy });
+    const totalItems = await BookModel.countDocuments(query);
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+      totalItems,
+      totalPages: Math.ceil(totalItems / limit),
+      limit: +limit,
+      currentPage: page,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-// app.get("/api/v1/books/:id", async (req, res) => {
-//   try {
-//     const data = await BookModel.findById(req.params.id);
+app.get("/api/v1/books/:id", async (req, res) => {
+  try {
+    const data = await BookModel.findById(req.params.id);
 
-//     if (data) {
-//       return res.status(200).json({
-//         msg: "Ok",
-//         data,
-//       });
-//     }
+    if (data) {
+      return res.status(200).json({
+        msg: "Ok",
+        data,
+      });
+    }
 
-//     return res.status(404).json({
-//       msg: "Not Found",
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
+    return res.status(404).json({
+      msg: "Not Found",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-// app.post("/api/v1/books", async (req, res) => {
-//   try {
-//     const { name, author, price, description } = req.body;
-//     const book = new BookModel({
-//       name,
-//       author,
-//       price,
-//       description,
-//     });
-//     const data = await book.save();
-//     return res.status(200).json({
-//       msg: "Ok",
-//       // data,
-//       results: data,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
+app.post("/api/v1/books", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const book = new BookModel({
+      name,
+      author,
+      price,
+      description,
+    });
+    const data = await book.save();
+    return res.status(200).json({
+      msg: "Ok",
+      // data,
+      results: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-// app.put("/api/v1/books/:id", async (req, res) => {
-//   try {
-//     const { name, author, price, description } = req.body;
-//     const { id } = req.params;
+app.put("/api/v1/books/:id", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const { id } = req.params;
 
-//     const data = await BookModel.findByIdAndUpdate(
-//       id,
-//       {
-//         name,
-//         author,
-//         price,
-//         description,
-//       },
-//       { new: true }
-//     );
+    const data = await BookModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        author,
+        price,
+        description,
+      },
+      { new: true }
+    );
 
-//     return res.status(200).json({
-//       msg: "Ok",
-//       data,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
-// app.delete("/api/v1/books/:id", async (req, res) => {
-//   try {
-//     await BookModel.findByIdAndDelete(req.params.id);
-//     return res.status(200).json({
-//       msg: "Ok",
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
+app.delete("/api/v1/books/:id", async (req, res) => {
+  try {
+    await BookModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json({
+      msg: "Ok",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
 const PORT = process.env.PORT;
 
