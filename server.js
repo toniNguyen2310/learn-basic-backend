8e7d9481f58c06e2f0aa474ab1de47b046c08cb6
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-08-28 17:16:04 +0700

    updatr vercel

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-// require("dotenv").config(); //để sử dụng process env
-// const express = require("express");
-// const configViewEngine = require("./src/config/viewEngine");
-// const webRoutes = require("./src/routes/web");
-// const connection = require("./src/config/database");
+require("dotenv").config(); //để sử dụng process env
+const express = require("express");
+const configViewEngine = require("./src/config/viewEngine");
+const webRoutes = require("./src/routes/web");
+const connection = require("./src/config/database");
 
-// const app = express();
-// const port = process.env.PORT || 8888;
-// const hostname = process.env.HOST_NAME;
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 //config
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //config template engine
-// configViewEngine(app);
+configViewEngine(app);
 
 //khai báo route
-// app.use("/", webRoutes);
+app.use("/", webRoutes);
 
 //test connection
-// (async () => {
-//   try {
-//     await connection();
-//     app.listen(port, hostname, () => {
-//       console.log(`backend zero app listening on port ${port}`);
-//     });
-//   } catch (error) {
-//     console.log("Error connect to DB>> ", error);
-//   }
-// })();
+(async () => {
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`backend zero app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Error connect to DB>> ", error);
+  }
+})();
