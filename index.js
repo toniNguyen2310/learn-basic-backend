15be6f71ccadec963082b51fdefa4a010067eb38
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-08-29 19:37:57 +0700

    update route

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ require("dotenv").config(); //để sử dụng process env
 const express = require("express");
 const configViewEngine = require("./src/config/viewEngine");
 const webRoutes = require("./src/routes/web");
+const apiRoutes = require("./src/routes/api");
 const connection = require("./src/config/database");
-const { getHomePage } = require("./src/controllers/homeControllers");
+// const { getHomePage } = require("./src/controllers/homeControllers");
 
 const app = express();
-app.use(express.json());
 
 const hostname = process.env.HOST_NAME;
 // connection();
 //config
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //config template engine
 configViewEngine(app);
 //khai báo route
-app.get("/", (req, res) => {
-  res.send("ok");
-});
-// app.use("/", webRoutes);
+// app.get("/", (req, res) => {
+//   res.send("ok");
+// });
+
 const port = process.env.PORT;
 //test connection
 // app.listen(port, () => {
@@ -37,3 +37,6 @@ const port = process.env.PORT;
     console.log("Error connect to DB>> ", error);
   }
 })();
+
+// app.use("/v1/api", webRoutes);
+app.use("/v1/api", apiRoutes);
